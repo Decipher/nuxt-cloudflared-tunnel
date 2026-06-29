696240db267925a1a7d8fb1058e86fc8222bde88
Stuart Clark <stu@rtclark.net>
2026-06-29 15:55:21 +1000

    feat: add Cloudflare icon and playground favicon
    - icon.svg: official Cloudflare cloud mark for nuxt/modules registry
    - playground/public/favicon.svg: same icon served as playground favicon
    - playground/nuxt.config.ts: wire up SVG favicon via app.head

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,6 +2,11 @@ export default defineNuxtConfig({
   modules: ['../src/module'],
   devtools: { enabled: true },
   compatibilityDate: '2026-06-29',
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+    },
+  },
   cloudflaredTunnel: {
     // Also tunnel a Storybook dev server on :6006. Only enabled when Storybook
     // is actually running alongside Nuxt (use `pnpm dev:all`, which sets
